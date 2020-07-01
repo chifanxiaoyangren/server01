eca2e0dc8d56eaa1b795b3a1586af137f6450c18
root <root>
2020-07-01 16:04:31 +0800

    add btn

--- WindowsFormsApp28/Form1.Designer.cs
+++ WindowsFormsApp28/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -39,11 +40,21 @@
             this.textBox1.Size = new System.Drawing.Size(321, 310);
             this.textBox1.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 177);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +66,7 @@
 
         #endregion
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
